cc59043a684185d49a4bfb6408a15ba3fb426cfd
Devansh-Agarwal <thedevanshagarwal@gmail.com>
2018-02-27 01:13:53 +0530

    assets

---
+++ Assets/movie.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movie : MonoBehaviour {
+
+	public MovieTexture mov;// Use this for initialization
+	void Start () {
+		GetComponent<Renderer> ().material.mainTexture = mov;
+		mov.Play ();
+		mov.loop =true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
